d64111f4b49f793bc1e9bf28d4e9d22e267956bb
RafeHatfield <rafe.hatfield@suite101.com>
2013-02-14 17:16:53 -0800

    added local settings file and wrapped settings for memcache in an environment check

---
+++ sites/default/local.settings.php
@@ -0,0 +1,13 @@
+<?php
+
+ini_set('memory_limit', '256M');
+ini_set('error_reporting', E_ALL);
+
+// $databases['default']['default'] = array(
+//  'driver' => 'mysql',
+//  'database' => 'suite101comdev',
+//  'username' => 'root',
+//  'password' => 'suite101',
+//  'host' => 'localhost',
+//  'prefix' => '',
+// );
